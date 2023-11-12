93cf65b3a026d5803cb246123ab922c466e3ee83
villageworks <villageworks@gmail.com>
2023-11-13 00:44:21 +0900

    ver.0.11: 商品追加機能の実装

--- src/main/java/com/example/demo/controller/CartController.java
+++ src/main/java/com/example/demo/controller/CartController.java
@@ -1,13 +1,31 @@
 package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.example.demo.model.Item;
 
 @Controller
 public class CartController {
 	// 初期表示
 	@GetMapping("/cart")
 	public String showCart() {
+		return "cart";
+	}
+	
+	@PostMapping("/cart/add")
+	public String addCart(
+			@RequestParam("name") String name,
+			@RequestParam("price") int price,
+			Model model) {
+		// リクエストパラメータをもとに商品クラスのインスタンスを生成
+		Item item = new  Item(name, price);
+		//商品クラスのインスタンスをスコープに登録
+		model.addAttribute("item", item);
+		// 画面遷移
 		return "cart";
 	}
 }
